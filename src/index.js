a57f60a8cc4d5949b97c1094f6b37f9db44f8a05
yethan <yethan444@gmail.com>
2024-06-17 18:22:16 +0200

    CodeBook Deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { FilterProvider, CartProvider } from './context';
+import { ScrollToTop } from './components';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <CartProvider>
+      <FilterProvider>
+        <ScrollToTop />
+        <ToastContainer closeButton={false} autoCloe={3000}/>
+        <App />
+      </FilterProvider>
+    </CartProvider>
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1E293B",
+      }
+    },
+  },
+  plugins: [],
+}
+
