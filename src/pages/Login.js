9190639eae710413bb5973b02f4e04d2f35f7922
yethan <yethan444@gmail.com>
2024-06-18 10:27:41 +0200

    Login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,6 +28,25 @@ export const Login = () => {
     }
   }
 
+  async function handleLoginGuest(){
+    email.current.value = "example@example.com";
+    password.current.value = "example123";
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch (error) {
+      toast.error(error.message, {
+        closeButton: true,
+        closeOnClick: true,
+        position: "top-center",
+      });
+    }
+  }
+
   return (
     <main>
       <section>
@@ -75,6 +94,7 @@ export const Login = () => {
           Log In
         </button>
       </form>
+      <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   );
 };
